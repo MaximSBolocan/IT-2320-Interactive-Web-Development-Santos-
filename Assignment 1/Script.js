e27e802e20aa6ec6a1c83e4defdb553a01b0383b
MaximSBolocan <wolverene1@gmail.com>
2016-09-17 17:55:17 -0400

    Some changes

--- Assignment 1/Script.js
+++ Assignment 1/Script.js
@@ -1,68 +1,11 @@
-function BigButton1()
+function BigButton(x)
 {
-	var a=document.getElementsByClassName("button1");
-	
-	var b=document.getElementsByClassName("big");
-	var x=a[0];
-	
-	if(x!="big")
-	{
-	x.className="big";
-	}
-
-	else
-	{
-		x.className="button1";
-	}	
+	x.className="big-Button";
 	
 }
-function BigButton2()
-{
-	var a=document.getElementsByClassName("button2");
-	
-	var b=document.getElementsByClassName("big");
-	var x=a[0];
-	
-	if(x!="big")
-	{
-	x.className="big";
-	}
 
-	else
-	{
-		x.className="button2";
 		
-	}
-	
-}
-		
-function Normal1()
+function Normal(x)
 {
-	var a=document.getElementsByClassName("button1");
-	var b=document.getElementsByClassName("big");
-	var x=b[0];
-	if(x!="button1")
-	{
-	x.className="button1";
-	}
-	else
-	{
-	x.className="big";
-	}
-}
-function Normal2()
-{
-	var a=document.getElementsByClassName("button2");
-	var b=document.getElementsByClassName("big");
-	var x=b[0];
-
-	if(x!="button2")
-	{
-	x.className="button2";
-	}
-	else
-	{
-	
-	x.className="big";
-	}
+	x.className="button";
 }
