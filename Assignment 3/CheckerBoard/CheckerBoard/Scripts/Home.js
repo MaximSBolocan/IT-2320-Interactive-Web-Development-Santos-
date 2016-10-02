42b242ad708852c5d626e88913be920e2fabb5dd
MaximSBolocan <wolverene1@gmail.com>
2016-10-02 18:44:45 -0400

    Fsome changes  not done

---
+++ Assignment 3/CheckerBoard/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+
+    //alert("pagina so reinoit");
+    var cells = $(".cell");
+    var colorCount = 0;
+    //var Piece=$(".piece")
+
+    for (var i = 0; i < cells.length; i++) {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+    
+
+    mainPage.highlightEvent();
+   
+   
+});
+
+var mainPage = {};
+
+mainPage.highlightEvent= function()
+{
+    var newPiece=null;
+    var curentpiece = $(".piece");
+    
+    curentpiece.click(function()
+    {
+       if (curentpiece!=newPiece)
+        {
+          $(this).css("border", "3px solid black");
+          newPiece = curentpiece;
+        }
+        else  
+        {
+            curentpiece = curentpiece.css("border", "");
+            newPiece=null;
+        }
+        
+    })
+
+
+    
+
+    
+     
+}
