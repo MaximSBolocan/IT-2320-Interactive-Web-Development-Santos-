b2ac29fb3291b195e26122ea22b683f3fcb22a6a
MaximSBolocan <wolverene1@gmail.com>
2016-10-12 20:54:45 -0400

    Update

--- Assignment 4/Script.js
+++ Assignment 4/Script.js
@@ -10,24 +10,55 @@ MainPage.Team = function ( teamName)
  
  
  }
+ MainPage.Team.prototype.SetTeamName=function(teamName )
+ {
+	 this.TeamName=teamName;
+ }
  // The PLayer Constructor !!!!!!!!!!!!!
  MainPage.Player=function(name)
 {
 	
 	this.LastName= name ;
 }
-MainPage.TEamRed=new MainPage.Team("Red");//new team
+///////////////////////////////////////////////////////////////////////////////
+MainPage.TEamRed=new MainPage.Team();//new team
+MainPage.TEamRed.SetTeamName("TEamRed");//creating name for the team 
+//creation of new players in the new team 
 MainPage.TEamRed.Players.IronMan=new MainPage.Player("Iron Man");
 MainPage.TEamRed.Players.SpiderMan =new MainPage.Player("SpiderMan");
 MainPage.TEamRed.Players.Hulk =new MainPage.Player("Hulk");
 MainPage.TEamRed.Players.BlackWidow =new MainPage.Player("Blck Widow");
-document.body.innerHTML=MainPage.TEamRed.Players.IronMan.LastName; 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+MainPage.TEamBlue=new MainPage.Team();//new team
+MainPage.TEamBlue.SetTeamName("Team Blue");//creating name for the team 
+//creation of new players in the new team 
+MainPage.TEamBlue.Players.CaptainAmerica=new MainPage.Player("Captain America");
+MainPage.TEamBlue.Players.WinterSolder =new MainPage.Player("Winter Solder");
+MainPage.TEamBlue.Players.Hawkeye =new MainPage.Player("Hawkeye");
+MainPage.TEamBlue.Players.AntMan =new MainPage.Player("Ant-Man");
+////////////////////////////////////////////////////////////////////////////////
+document.body.innerHTML=MainPage.TEamRed.TeamName;
+document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamRed.Players.IronMan.LastName; 
 document.body.innerHTML += "<br/>"
 document.body.innerHTML+=MainPage.TEamRed.Players.SpiderMan.LastName; 
 document.body.innerHTML += "<br/>"
 document.body.innerHTML+=MainPage.TEamRed.Players.Hulk.LastName;
  document.body.innerHTML += "<br/>"
 document.body.innerHTML+=MainPage.TEamRed.Players.BlackWidow.LastName;
+ document.body.innerHTML += "<br/>"
+ document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamBlue.TeamName;
+document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamBlue.Players.CaptainAmerica.LastName; 
+document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamBlue.Players.WinterSolder.LastName; 
+document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamBlue.Players.Hawkeye.LastName;
+ document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamBlue.Players.AntMan.LastName;
+
+ document.body.innerHTML += "<br/>"
 
 // MainPage.Team ={firstPlayer:"Iron Man",secondPlayer:"Hulk",thirdPlayer:"SpiderMan",fouthPlayer:"Black Widow"}
 // document.body.innerHTML =MainPage.Team.firstPlayer;
