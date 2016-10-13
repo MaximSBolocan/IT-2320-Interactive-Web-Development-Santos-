4c0321313702a99ef606871d0a097f5ad91e05bb
MaximSBolocan <wolverene1@gmail.com>
2016-10-12 20:11:00 -0400

    Update

--- Assignment 4/Script.js
+++ Assignment 4/Script.js
@@ -1,50 +1,99 @@
  var MainPage={};
  
- MainPage.Team ={firstPlayer:"Iron Man",secondPlayer:"Hulk",thirdPlayer:"SpiderMan",fouthPlayer:"Black Widow"}
-document.body.innerHTML =MainPage.Team.firstPlayer;
-document.body.innerHTML += "<br/>"
-document.body.innerHTML+=MainPage.Team.secondPlayer;
-document.body.innerHTML += "<br/>"
-document.body.innerHTML +=MainPage.Team.thirdPlayer;
-document.body.innerHTML += "<br/>"
-document.body.innerHTML +=MainPage.Team.fouthPlayer;
-document.body.innerHTML += "<br/>"
-/////////////////////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////////////////////
+ 
 
-// MainPage.Team = function ( teamName) 
-// {  
- // this.TeamName =teamName;
- // this.persons= new MainPage.Player();
- // this.persons.SetName(steve);
+// The team Constructor !!!!!!!
+MainPage.Team = function ( teamName) 
+{  
+ this.TeamName =teamName;
+ this.Players= {};
+ 
+ 
+ }
+ // The PLayer Constructor !!!!!!!!!!!!!
+ MainPage.Player=function(name)
+{
+	
+	this.LastName= name ;
+}
+MainPage.TEamRed=new MainPage.Team("Red");//new team
+MainPage.TEamRed.Players.IronMan=new MainPage.Player("Iron Man");
+MainPage.TEamRed.Players.SpiderMan =new MainPage.Player("SpiderMan");
+MainPage.TEamRed.Players.Hulk =new MainPage.Player("Hulk");
+MainPage.TEamRed.Players.BlackWidow =new MainPage.Player("Blck Widow");
+document.body.innerHTML=MainPage.TEamRed.Players.IronMan.LastName; 
+document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamRed.Players.SpiderMan.LastName; 
+document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamRed.Players.Hulk.LastName;
+ document.body.innerHTML += "<br/>"
+document.body.innerHTML+=MainPage.TEamRed.Players.BlackWidow.LastName;
+
+// MainPage.Team ={firstPlayer:"Iron Man",secondPlayer:"Hulk",thirdPlayer:"SpiderMan",fouthPlayer:"Black Widow"}
+// document.body.innerHTML =MainPage.Team.firstPlayer;
+// document.body.innerHTML += "<br/>"
+// document.body.innerHTML+=MainPage.Team.secondPlayer;
+// document.body.innerHTML += "<br/>"
+// document.body.innerHTML +=MainPage.Team.thirdPlayer;
+// document.body.innerHTML += "<br/>"
+// document.body.innerHTML +=MainPage.Team.fouthPlayer;
+// document.body.innerHTML += "<br/>"
+// document.body.innerHTML+=Team.getfirstplayer();
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+// MainPage.prototype.getfirstplayer=function()
+// {
+	// return this.firstPlayer;
+// }
+// MainPage.Team.prototype.adPlayer =function(function)
+// {
+	// this.persons=function;
+// }
+
+
+ // MainPage.Team.prototype.GetTeamName = function()
+ // {
+	//return this.TeamName;
  // }
 
+// MainPage.Steve= new MainPage.Player("Steve");
+// MainPage.Max= new MainPage.Player("Max");
+// MainPage.RedTeam =new MainPage.Team("Red Team");
+// MainPage.RedTeam.persons =new MainPage.Player("steve")
+// MainPage.RedTeam.persons=MainPage.Steve,MainPage.Max.;
+//MainPage.RedTeam.persons=MainPage.Max.LastName;
 
 
-// MainPage.Team.prototype.GetTeamName = function()
+// MainPage.Team.prototype.GetPlayerName=function()
 // {
-	// return this.TeamName;
+	// this.Players.Player.prototype.getName= function()
+	// {
+		// return this.LastName;
+	// }
 // }
-// MainPage.Player=function()
-// {
-	
-	// this.LastName;
-// }
+
+
+
 // MainPage.Player.prototype.SetName=function(name)
 // {
 	
 	// this.LastName=name
 // }
+// MainPage.Player.prototype.SetNam =function(name)
+// {
+	 // this.LastName= name ;
+// }
 
-
-// MainPage.Player.prototype.GetName =function()
+// MainPage.Team.prototype.Players.MainPage.Player.prototype.GetName =function()
 // {
 	// return this.LastName;
 // }
- // MainPage.TeamA= new MainPage.Team("TeamA");
-//MainPage.TeamA.persons.SetName(steve)
-// alert("")
-
+  // MainPage.Clevelan= new MainPage.Team("Cleveland");
+  // MainPage.Chcago = new MainPage.Team("Chicago");
+  // MainPage.LeBrone=new MainPage.Player();
+  // MainPage.Cleveland.adPlayer(MainPage.LeBrone=new MainPage.Player())
+alert("")
+// document.body.innerHTML = MainPage.Clevelan.TeamName;
 //document.body.innerHTML = MainPage.TeamA.steve.GetName();
 //document.body.innerHTML += "<br/>"
 //document.body.innerHTML += MainPage.Bob.GetName();
