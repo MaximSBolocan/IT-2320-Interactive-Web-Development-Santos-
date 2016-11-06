d5bba8b142c855440fbf690a35154cdc1353c84e
MaximSBolocan <wolverene1@gmail.com>
2016-11-06 18:07:01 -0500

    Inputs

---
+++ Final Project/FinalProject/Scripts/Home.js
@@ -0,0 +1,31 @@
+﻿var Main = {};
+$(document).ready(function () {
+    $("#login").click(Main.createLogin)
+    $("#acount").click(Main.createAccount)
+});
+Main.createLogin = function () {
+    $("#login").remove();
+    $("#acount").remove();
+    var login = "<input type='text'value='UserName'> <br><input type='text' value='Password'>"
+    var button = "<button onclick='alert(Main.alertLogin)'>Submit</button>"
+    $("#Main").prepend(login, button);
+}
+ Main.createAccount=function()
+{
+    $("#login").remove();
+    $("#acount").remove();
+    var createAcout = "<input type='text'value='UserName'> <br><input type='text' value='Password'>"
+    var createEmail = "<input type='text'value='Email'> <br><input type='text' value='ConfirmEmail'>"
+    var button = "<button onclick='alert(Main.alertCreateAccount)'>Submit</button>"
+    $("#Main").prepend(createAcout, createEmail,button);
+    
+ }
+ Main.alertLogin=function()
+ {
+     alert("Login")
+ }
+ Main.alertCreateAccount=function()
+ {
+     alert("Account Create ")
+ }
+
