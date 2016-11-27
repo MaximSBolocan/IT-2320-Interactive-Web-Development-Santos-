17f44312006478e7152cb8a38346e73c88651a64
MaximSBolocan <wolverene1@gmail.com>
2016-11-27 16:41:30 -0500

    Working

--- Final Project/FinalProject/Scripts/Home.js
+++ Final Project/FinalProject/Scripts/Home.js
@@ -10,24 +10,24 @@ Main.createLogin = function () {
     $("#login").remove();
     $("#account").remove();
     $("#button").remove();
-    var login = "User Name <br><input type='text' value='' id='text' min='6'> <br> Password<br> <input type='password' value='' min='6' id='password'>"
-    var button = "<br><button  onclick='Main.Login()'>Login</button>"
+    var login = "User Name <br><input type='text' value='' id='text1' min='6'> <br> Password<br> <input type='password' value='' min='6' id='password'><br><button  onclick='Main.Login()'>Login</button> "
+   
 
-    $("#text").prepend(login, button);
+    $("#info").prepend(login);
 }
  Main.createAccount=function()
 {
     $("#login").remove();
     $("#account").remove();
-    var createAcout = "User Name <input type='text' value='' id='text' min='6' placeholder='at least 6 caracters '> <br>Password<br><input type='password' value='' id='password'min='6' placeholder='at least 6 caracters ' >"
-    var createEmail = "<br>Email<input type='email' value='' id='email'> <br>Email Confirm<input type='email' value='' id='emailC'>"
-    var button = "<br><button onclick='Main.CreateAccount()'>Create</button>"
-    $("#text").prepend(createAcout, createEmail,button);
+    var createAcout = "User Name <input type='text' value='' id='text' min='6' placeholder='at least 6 caracters '> <br>Password<br><input type='password' value='' id='password'min='6' placeholder='at least 6 caracters ' ><br>Email<br><input type='email' value='' id='email'> <br>Email Confirm<br><input type='email' value='' id='emailC'><br><button onclick='Main.CreateAccount()'>Create</button>"
+
+    
+    $("#info").prepend(createAcout);
     
  }
  Main.Login = function () {
-
-     var userName = $('#text[value=""]').val()
+     
+     var userName = $('#text1[value=""]').val()
      var password = $('#password[value=""]').val()
      $.ajax
       ({
@@ -41,168 +41,191 @@ Main.createLogin = function () {
           success: function (response) {
 
               var deserializedData = JSON.parse(response);
-              var textMessage = deserializedData.Message
-              var textUser = deserializedData.Username;
-              var textPassword = deserializedData.Password;
-              $("#text").remove()
-              /*$(".empty").prepend(textMessage);
-              $(".empty").prepend(textUser);
-              $(".empty").prepend(textPassword);*/
-              
-              Main.AccountInfo(userName);
+              var name = deserializedData.Username;
+              var message = deserializedData.Message;
+              var password = deserializedData.Password;
+              var control2 = "Success";
+              $("#info").remove();
+              $("#text").append("<div id='info'></div>")
+              if (message == control2) {
+                  alert("succes")
+                  Main.AccountInfo(userName)
 
-
-          }
-
-      })
- }
-  Main.CreateAccount = function () {
-      var userName = $('#text[value=""]').val()
-      var password = $('#password[value=""]').val()
-      var email=$('#email[value=""]').val()
-      var emailCo=$('#emailC[value=""]').val()
-
-         $.ajax
-          ({
-              url: "Home/CreateAccount",
-              data:
-                    {
-                        Username: userName,
-                        Password: password,
-                        EmailAdd: email,
-                        EmailCon: emailCo
-                    },
-
-              success: function (response)
-              {
-                  var deserializedData = JSON.parse(response);
-                  var button = "<br><button id='button' name='Login'>Login</button>"
-                  $("#text").empty();
-                  $(".empty").append(deserializedData.Message);
-                  $(".empty").append(deserializedData.Username);
-                  $(".empty").append(deserializedData.Password);
-                  $(".empty").append(deserializedData.EmailAdd);
-                  $(".empty").append(deserializedData.EmailCon);
-                  $(".empty").append(button)
-                  $("#button").click(Main.createLogin);
-                  
               }
-          })
-         
-  }
-  Main.AccountInfo=function(name)
-  {
-      var user = name;
-      var userName;
-      var password;
-      var email;
-      
-      $.ajax
-       ({
-           url: "Home/GetAccountInformation",
-           data:
-                 {
-                     Username: user,
-                 },
-           success: function (response) {
-               var deserializedData = JSON.parse(response);
-               var button = "<br><button id='button' name='Update'>Update</button>"
-               var textError ="Error :" + deserializedData.Error + "<br>";
-               var textMessage ="Message:" + deserializedData.Message + "<br>";
-               var textPayload = JSON.parse(deserializedData.Payload);
-                userName = textPayload.account.username;
-                password = textPayload.account.password;
-                email = textPayload.account.emailadd;
-   /////////////////////////////////////////////////////////////////////////////////////////
-               $(".empty").append("<p>User Name:<input type='text' id='user' value='' class='form' readonly></p> ");
-               $("#user").val(userName);
-               $(".empty").append("<p>Password:<input type='text' id='password' value=''class='form' ></p>");
-               $("#password").val(password);
-               $(".empty").append("<p>Email:<input type='text' id='email' value=''class='form'  ></p>");
-               $("#email").val(email);
-               $(".empty").append(textError, textMessage, button);
-               $("#button").click(UpdateCall);
-               ////////////////////////////////////////////////////////////////////////////////
-           }
-       })
-      
+              else
+              {
+                  alert("wrong password or user name")
+                  var page = "<div id='info'><div  id='login'>Login</div><div  id='account'> Create Acount</div></div>"
+                  $("#info").remove();
+                  $("#text").prepend(page);
+                  $("#login").click(Main.createLogin)
+                  $("#account").click(Main.createAccount)
 
-      UpdateCall = function () {
-          alert("begin")
-          var tag1 = "password"
-          var tag2 = "emailadd"
-          if ($('#email[value=""]').val() != email && $('#password[value=""]').val() != password)
-          {
-              var newEmail = $('#email[value=""]').val();
-              var newPasword = $('#password[value=""]').val();
-              Main.UpdateAccount(userName, tag1, newPasword);
-              Main.UpdateAccount(userName, tag2, newEmail)
-           }
-      if ($('#password[value=""]').val() != password) {
-          alert("diferentpass")
-          var newPasword = $('#password[value=""]').val();
-          Main.UpdateAccount(userName,tag1, newPasword);
+              }
+            
           }
-          
-      if ($('#email[value=""]').val() != email)
-          {
-          alert("diferentmail")
-          var newEmail = $('#email[value=""]').val();
-          Main.UpdateAccount(userName,tag2, newEmail)
-      }
-      
-      
-      $("#button").remove();
-      $("p").remove();
-      }
+      })
+
+ }
+ 
+ Main.AccountInfo = function (name) {
+     var user = name;
+     var userName;
+     var password;
+     var email;
+     var button2 = "<br><button id='button2' name='Update'>Update</button>"
      
 
-  }
-  Main.UpdateAccount= function(name,elementname,elementvalue)
-  {
-
-     alert("start")
-      var user = name;
-      var elementName=elementname;
-      var elementValue=elementvalue;
-
-      $.ajax
+     $.ajax
       ({
-          url: "Home/AddOrUpdateElement",
+          url: "Home/GetAccountInformation",
           data:
                 {
-                    Username: user,
-                    ElementName:elementName,
-                    ElementValue:elementValue
+                    Username:user,
                 },
-
           success: function (response) {
               var deserializedData = JSON.parse(response);
-              alert("im here")
-              var textError = "Error :" + deserializedData.Error + "<br>";
-              var textMessage = "Message:" + deserializedData.Message + "<br>";
+              
               var textPayload = JSON.parse(deserializedData.Payload);
               userName = textPayload.account.username;
               password = textPayload.account.password;
               email = textPayload.account.emailadd;
-              $("#user").remove()
-              $("#password").remove()
-              $("#email").remove()
-              /////////////////////////////////////////////////////////////////////////////////////////
-              $(".empty").append("User Name:<div  id='user' class='form' ></div> ");
+              $("#info").append("User Name:<div  id='user' class='form' ></div> ");
               $("#user").append(userName);
-              $(".empty").append("Password:<div  id='password' class='form' ></div>");
+              $("#info").append("Password:<div  id='password' class='form' ></div>");
               $("#password").append(password);
-              $(".empty").append("Email:<div id='email' class='form'  ></div>");
-              $("#email").append(email);
-              $(".empty").append(textError, textMessage);
+              $("#info").append("Email:<div id='email' class='form'  ></div>");
+              $("#email").prepend(email);
+              $("#info").append(button2);
+              $("#button2").click(Update)
               
-              /////////////////////////////////////////////////////////////////////////////////////////
+              
           }
+      })
+     
+     function Update() {
+         
 
+         var button3 = "<br><button id='button3' >Validate</button>"
+         /*alert("start1")*/
+         $("#info").remove()
+         $("#text").prepend("<div id='info'></div>")
+         $("#info").append("User Name:<div  id='user' class='form' ></div> ");
+         $("#user").append(userName);
+         $("#info").append("Password:<input type='text' id='password' value=''class='form' >");
+         $("#password").val(password);
+         $("#info").append("Email:<input type='text' id='email' value=''class='form'  >");
+         $("#email").val(email);
+         $("#info").append(button3)
+         $("#button3").click(Validate)
+         
+     }
+     
+         
+     function Validate() {
+             var tag1 = "password"
+             var tag2 = "emailadd"
+             /*alert("validating")*/
+            
+             
+             if ($('#password[value=""]').val() != password) {
+                 alert("diferentpass")
+                 var newPasword = $('#password[value=""]').val();
+                 Main.UpdateAccount(userName, tag1, newPasword);
+             }
+
+             if ($('#email[value=""]').val() != email) {
+                 alert("diferentmail")
+                 var newEmail = $('#email[value=""]').val();
+                 Main.UpdateAccount(userName, tag2, newEmail)
+             }
+             else {
+                 $("#info").remove()
+                 $("#text").append("<div id='info'></div>")
+                 Main.AccountInfo(userName)
+             }
+             
+         
+     }
+ }
+ Main.UpdateAccount = function (name, elementname, elementvalue) {
+
+     /*alert("start")*/
+     var user = name;
+     var elementName = elementname;
+     var elementValue = elementvalue;
+
+     $.ajax
+     ({
+         url: "Home/AddOrUpdateElement",
+         data:
+               {
+                   Username: user,
+                   ElementName: elementName,
+                   ElementValue: elementValue
+               },
+
+         success: function (response) {
+             var deserializedData = JSON.parse(response);
+             /*alert("im here")*/
+             var textError =  deserializedData.Error ;
+             var textMessage = deserializedData.Message;
+             var textPayload = JSON.parse(deserializedData.Payload);
+             userName = textPayload.account.username;
+             password = textPayload.account.password;
+             email = textPayload.account.emailadd;
+             $("#info").remove()
+             $("#text").append("<div id='info'></div>")
+             /////////////////////////////////////////////////////////////////////////////////////////
+      
+             Main.AccountInfo(userName)
+                    
+          /////////////////////////////////////////////////////////////////////////////////////////
+         }
+
+     })
+
+ }
+ 
+ Main.CreateAccount = function () {
+     var userName = $('#text[value=""]').val()
+     var password = $('#password[value=""]').val()
+     var email = $('#email[value=""]').val()
+     var emailCo = $('#emailC[value=""]').val()
+     /*alert("startCreating")*/
+     $.ajax
+      ({
+          url: "Home/CreateAccount",
+          data:
+                {
+                    Username: userName,
+                    Password: password,
+                    EmailAdd: email,
+                    EmailCon: emailCo
+                },
+
+          success: function (response) {
+              var deserializedData = JSON.parse(response);
+              var responseMessage = deserializedData.Message;
+              var userNameMessage=deserializedData.Username;
+              var PasswordMessage=deserializedData.Password;
+              var EmailAddMesage=deserializedData.EmailAdd;
+              var EmailConMessage=deserializedData.EmailCon;
+              var control = "Success"
+              $("#info").remove()
+              $("#text").append("<div id='info'></div>")
+              if(responseMessage==control)
+              {
+                  Main.AccountInfo(userName)
+              }
+              else
+              {
+                  alert(" "+responseMessage+" User Name is "+userNameMessage+" Password is "+PasswordMessage+" Email adress is "+EmailAddMesage+" Email Con is "+EmailConMessage)
+                  Main.createAccount()
+              }
+              
+
+          }
       })
 
-  }
-  
- 
-
+ }
